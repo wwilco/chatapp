49abf1a9e12d4e63f4802d1b7f732dbf0ab178e4
wwilco <wgmwilcox@gmail.com>
2014-12-12 21:04:46 -0500

    liz take

--- srv.js
+++ srv.js
@@ -1,10 +1,12 @@
 var Websocket = require("ws").Server;
 var server = new Websocket({port:3000});
 var clients = [];
+var history = [];
 server.on("connection", function(ws){
   clients.push(ws);
   clients.forEach(function(client){
-    client.send("Someone has joined the chatroom! Neato!");
+    client.send("you joined the chat!");
+    //ws.send(hist);
 
     //  setInterval(function(ws){
     //  client.send("Hello!");
@@ -16,12 +18,21 @@ server.on("connection", function(ws){
     console.log(message);
     for (i=0;i<clients.length;i++){
       clients[i].send(message);
+      //history.send(message);
     }
   })
+  ws.on("message", function(message){
+    //history.push(message);
+    //console.log(message);
+    //ws.send(message);
+  })
+  var hist = history.join("\n"); //makes a break in a string
+  //ws.send(hist);
+
   ws.on("close",function(){
     var x = clients.indexOf(ws);
     clients.splice(x,1);
-    console.log(clients.name + " clients are  still in the room");
+    console.log(clients.length + " clients are  still in the room");
     clients.forEach(function(client){
       client.send("Oh no someone left!");
     })
